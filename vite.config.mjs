7dc1f4b97e1339f47be3a551c44ab1eba0b000df
c_r_5 <48353236+c-r-5@users.noreply.github.com>
2026-02-20 16:01:34 +0900

    Fix vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -14,7 +14,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/',
   plugins: [
     VueRouter(),
     Layouts(),
